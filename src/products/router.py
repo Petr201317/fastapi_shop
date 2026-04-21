204b728428a0e9a35d2ed8af6e01659ea2bea9b9
Petr201317 <petrdrachen1@gmail.com>
2026-04-21 20:15:20 +0300

    small fixes

--- src/products/router.py
+++ src/products/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from .depends import get_products_service
 from .schemas import CreateProductFormSchema
 from src.auth.depends import get_current_user
@@ -7,7 +7,12 @@ router = APIRouter(prefix="/products", tags=["products"])
 
 @router.post("/create")
 async def create_product(credentials: CreateProductFormSchema, current_user = Depends(get_current_user), service = Depends(get_products_service)):
-    return await service.add_product(credentials, current_user)
+    res = await service.add_product(credentials, current_user)
+    if not res:
+        raise HTTPException(status_code=401)
+    else:
+        return res
+
 
 
 
