7ec5d56d281f56831f716c2384d762f8c775f91c
Petr201317 <petrdrachen1@gmail.com>
2026-04-21 15:36:27 +0300

    add alembic

--- src/db/database.py
+++ src/db/database.py
@@ -7,11 +7,6 @@ async_engine = create_async_engine(
     url=db_settings.db_url_async,
     echo=True,
 )
-sync_engine = create_engine(
-    url=db_settings.db_url,
-    echo=False,
-)
-
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
@@ -20,16 +15,6 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async_session = async_sessionmaker(async_engine, expire_on_commit=False)
     async with async_session() as session:
         yield session
-
-def get_sync_session():
-    return sessionmaker(sync_engine, expire_on_commit=False)
-
-
-def init_database() -> list[str]:
-    Base.metadata.drop_all(sync_engine)
-    Base.metadata.create_all(sync_engine)
-    return inspect(sync_engine).get_table_names()
-
 
 class Base(DeclarativeBase):
     pass
