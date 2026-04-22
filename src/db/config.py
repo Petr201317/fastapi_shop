5450ed9036dce0c146093b5d3014024178ee5d7c
andoh <solomonkingsman@gmail.com>
2026-04-22 13:01:24 +0000

    fix .env path errors

--- src/db/config.py
+++ src/db/config.py
@@ -18,7 +18,9 @@ class DbSettings(BaseSettings):
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(
-        env_file=Path(__file__).parent / ".env", env_file_encoding="utf-8"
+        env_file=Path(__file__).parent.parent.parent / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
     )
 
 
