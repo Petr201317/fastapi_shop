c4d1fcc00dad61a4df4178556853ba2488eec16d
andoh <solomonkingsman@gmail.com>
2026-04-22 12:49:19 +0000

    add model relations

--- src/products/models.py
+++ src/products/models.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-import datetime
+from typing import TYPE_CHECKING, List
+from datetime import datetime
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, Numeric, Text, func, DateTime
 
 from src.db.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from decimal import Decimal
 
 if TYPE_CHECKING:
     from src.auth.models import UsersOrm
+    from src.orders.models import OrderItemsOrm
 
 
 class ProductsOrm(Base):
@@ -16,12 +18,19 @@ class ProductsOrm(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    name: Mapped[str]
-    description: Mapped[str]
-    price: Mapped[float]
-    image_url: Mapped[str]
+    name: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(Text)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    image_url: Mapped[str] = mapped_column(String(500))
 
-    created_by_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    created_by_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE")
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), insert_default=func.now()
+    )
+
+    # relations
     created_by: Mapped["UsersOrm"] = relationship(back_populates="created_products")
-
-    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now)
+    order_items: Mapped[List["OrderItemsOrm"]] = relationship(back_populates="product")
