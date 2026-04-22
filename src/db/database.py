ffc9cfc82ee468693288348605f7b70613410ef0
andoh <solomonkingsman@gmail.com>
2026-04-22 16:22:57 +0000

    move sessionmaker singleton out session getter

--- src/db/database.py
+++ src/db/database.py
@@ -3,18 +3,21 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine, inspect
 from .config import db_settings
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import AsyncGenerator
+
 async_engine = create_async_engine(
     url=db_settings.db_url_async,
     echo=True,
 )
 
-from sqlalchemy.ext.asyncio import AsyncSession
-from typing import AsyncGenerator
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
+
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async_session = async_sessionmaker(async_engine, expire_on_commit=False)
     async with async_session() as session:
         yield session
+
 
 class Base(DeclarativeBase):
     pass
